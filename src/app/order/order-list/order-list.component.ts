510c09ee29f4ae5d37c5e08c089499dc91b24568
Dayan Eam <eam.dayan@gmail.com>
2022-08-09 00:19:39 +0700

    create order list

--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
 })
 export class OrderListComponent implements OnInit {
+  orders = [
+    {
+      name: 'Ordered on yesterday',
+      group: 'HRD Center',
+    },
+    {
+      name: 'Ordered on 12/12/2022',
+      group: 'HRD Center',
+    },
+    {
+      name: 'Ordered on 14/12/2022',
+      group: 'MB',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
