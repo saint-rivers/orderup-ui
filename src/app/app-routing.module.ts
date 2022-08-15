ab8ff2e3b581ba6523c9b167f6e5ec17b2ab794f
dayan <eam.dayan@gmail.com>
2022-08-15 17:01:22 +0700

    fix flex grow in outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderFormComponent } from './order/order-form/order-form.component';
 import { OrderListComponent } from './order/order-list/order-list.component';
 
 const routes: Routes = [
   {
     path: "orders",
     component: OrderListComponent
+  },
+  {
+    path: "n/orders",
+    component: OrderFormComponent
   }
 ];
 
